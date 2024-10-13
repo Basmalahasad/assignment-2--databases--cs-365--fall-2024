9f0c4d53ff24061f0f09ae1f87db13f44a5ac8d4
Basmalah Asad <asad@hartford.edu>
2024-10-13 16:58:27 -0400

    setup AES_ENCRYPT

--- setup.sql
+++ setup.sql
@@ -4,5 +4,9 @@ CREATE DATABASE passwords DEFAULT CHARACTER SET utf8mb4;
 
 USE passwords;
 
+SET block_encryption_mode = 'aes-256-cbc';
+SET @key_str = UNHEX(SHA2('assignment 2', 512));
+SET @init_vector = RANDOM_BYTES(16);
+
 source create-tables.sql;
 source insert-values.sql;
