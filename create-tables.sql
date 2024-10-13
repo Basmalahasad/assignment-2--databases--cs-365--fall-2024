24ebcbc98eee1d6df902610662b99c432555df0a
Basmalah Asad <asad@hartford.edu>
2024-10-13 15:40:09 -0400

    create 'user' attributes

--- create-tables.sql
+++ create-tables.sql
@@ -1,3 +1,8 @@
 CREATE TABLE IF NOT EXISTS user (
-    
+    user_id SMALLINT(5) NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(128) NOT NULL,
+    last_name VARCHAR(128) NOT NULL,
+    username VARCHAR(128) NOT NULL UNIQUE,
+    email VARCHAR(128) NOT NULL UNIQUE,
+    PRIMARY KEY (user_id)
 );
