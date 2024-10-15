2ba28a46dbb752f9d8b212c6899924925e381c46
Basmalah Asad <asad@hartford.edu>
2024-10-14 22:15:51 -0400

    update create-tables.sql

--- create-tables.sql
+++ create-tables.sql
@@ -1,27 +1,27 @@
 CREATE TABLE IF NOT EXISTS user (
-    user_id SMALLINT(5) NOT NULL AUTO_INCREMENT,
+    user_id SMALLINT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(128) NOT NULL,
     last_name VARCHAR(128) NOT NULL,
-    username VARCHAR(128) NOT NULL UNIQUE,
     email VARCHAR(128) NOT NULL UNIQUE,
     PRIMARY KEY (user_id)
 );
 
 CREATE TABLE IF NOT EXISTS website (
-    website_id SMALLINT(5) NOT NULL AUTO_INCREMENT,
+    website_id SMALLINT NOT NULL AUTO_INCREMENT,
     website_name VARCHAR(128) NOT NULL,
     url VARCHAR(128) NOT NULL UNIQUE,
     PRIMARY KEY (website_id)
 );
 
-CREATE TABLE IF NOT EXISTS passwords (
-    password_id SMALLINT(5) NOT NULL AUTO_INCREMENT,
-    user_id SMALLINT(5) NOT NULL,
-    website_id SMALLINT(5) NOT NULL,
-    password VARBINARY(512) NOT NULL,
+CREATE TABLE IF NOT EXISTS credentials (
+    password_id SMALLINT NOT NULL AUTO_INCREMENT,
+    user_id SMALLINT NOT NULL,
+    website_id SMALLINT NOT NULL,
+    username VARCHAR(128) NOT NULL UNIQUE,
+    passphrase VARBINARY(512) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    comment TEXT,
-    PRIMARY KEY(password_id),
-    FOREIGN KEY (user_id) REFERENCES user(user_id),
-    FOREIGN KEY (website_id) REFERENCES website(website_id)
+    comments TEXT,
+    PRIMARY KEY (password_id),
+    FOREIGN KEY (user_id) REFERENCES user (user_id),
+    FOREIGN KEY (website_id) REFERENCES website (website_id)
 );
